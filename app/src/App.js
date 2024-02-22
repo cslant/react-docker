12ae7de011ce8afe304a857e7e546f884b147b79
thuankg1752 <thuankg1752@gmail.com>
2024-02-22 17:22:23 +0700

    add license

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,6 +16,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React with <span style={{color: 'red'}}>Docker</span>
+        </a>
+        <br/>
+        <a className="App-link"
+           href="https://github.com/cslant"
+           target="_blank"
+           rel="noopener noreferrer">
+          Docker setup by
+          <span style={{color: 'rgba(14, 116, 67, 1)'}}>
+            <strong>CSLANT</strong>
+          </span>
         </a>
       </header>
     </div>
